c0fe6067fdc9f093798ee8fc92e9ac12ce931b0b
Alexandr Fonov <FonovAD@Gmail.com>
2025-09-22 21:19:35 +0300

    MEEMO-1 Fix typo

--- internal/infradtructure/storage/pg/user/templates.go
+++ internal/infradtructure/storage/pg/user/templates.go
@@ -2,19 +2,22 @@ package user
 
 const (
 	CreateUserTemplate = `
-	INSERT INTO user 
-	VALUES ($1, $2, $3, $4, $5, $6, $7);`
+	INSERT INTO users 
+	VALUES ($1, $2, $3, $4, $5, $6, $7)
+	RETURNING id;`
 
 	GetUserByEmailTemplate = `
 	SELECT id, email, first_name, last_name, password_salt 
-	FROM user WHERE email = $1;`
+	FROM users WHERE email = $1;`
 
 	UpdateUserTemplate = `
-	UPDATE user 
+	UPDATE users
 	SET email = $2, first_name = $3, last_name = $4, password_salt = $5
-	WHERE email = $1;`
+	WHERE email = $1
+	RETURNING id;`
 
 	DeleteUserTemplate = `
-	DELETE FROM user
-	WHERE email = $1;`
+	DELETE FROM users
+	WHERE email = $1
+	RETURNING id;`
 )
